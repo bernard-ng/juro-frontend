3dba487e6dbd2eb97b184bb13b3f5926dd05e86b
bernard-ng <ngandubernard@gmail.com>
2024-05-06 20:04:59 +0200

    add api fetching module

---
+++ lib/api/model.ts
@@ -0,0 +1,50 @@
+
+export interface Response<T> {
+    data: T,
+    success: boolean,
+    code?: number,
+    description?: string
+}
+
+export interface User {
+    id: number,
+    email: string,
+    created_at: string,
+    updated_at: string
+}
+
+export interface Chat {
+    name: string,
+    created_at: string,
+    updated_at: string,
+    messages: Message[]
+}
+
+export interface Message {
+    id: number,
+    message: string,
+    created_at: string,
+    sender: "user" | "assistant" | "system"
+}
+
+export interface SuggestedPrompt {
+    id: number,
+    prompt: string,
+    title?: string,
+    created_at: string
+}
+
+export interface LoginRequestData {
+    username: string,
+    password: string
+}
+
+export interface LoginResponseData {
+    token: string
+}
+
+export interface RegisterRequestData {
+    email: string,
+    password: string
+    name: string
+}
