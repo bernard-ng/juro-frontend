2d455360b9660e5b7c79a502fd1cd5afaf80ee7e
bernard-ng <ngandubernard@gmail.com>
2024-05-10 00:50:12 +0200

    add field

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,10 @@
+"use client"
 import Image from "next/image";
 import type {Metadata} from "next";
 import Link from "next/link";
 import Card from "@/components/tailus-ui/Card";
-
-export const metadata: Metadata = {
-    title: "Connexion | Juro",
-    description: "Bon retour sur Juro, renseignez vos identifiants pour continuer !",
-};
+import Form from "tailus-ui/Form";
+import {Button} from "tailus-ui/Button";
 
 export default function Login() {
     return (
@@ -19,6 +17,49 @@ export default function Login() {
                         </a>
                         <h1 className="text-center text-2xl font-semibold text-gray-950 dark:text-white">Connectez-vous</h1>
                         <p className="text-center text-gray-700 dark:text-gray-400">Bon retour sur Juro, renseignez vos identifiants pour continuer !</p>
+
+                        <Form.Root>
+                            <div className="mb-6 mt-4 space-y-4">
+                                <Form.Field name="email">
+                                    <Form.Control asChild>
+                                        <Form.Input
+                                            variant="outlined"
+                                            type="email"
+                                            size="lg"
+                                            autoComplete="current-username"
+                                            placeholder="Votre adresse email"
+                                            aria-label="Email"
+                                            required
+                                        />
+                                    </Form.Control>
+                                    <Form.Message intent="warning" match="valueMissing">
+                                        Please enter your email
+                                    </Form.Message>
+                                    <Form.Message intent="danger" match="typeMismatch">
+                                        Please provide a valid email
+                                    </Form.Message>
+                                </Form.Field>
+                                <Form.Field name="password">
+                                    <Form.Control asChild>
+                                        <Form.Input
+                                            variant="outlined"
+                                            type="password"
+                                            autoComplete="current-password"
+                                            placeholder="votre mot de passe"
+                                            aria-label="Mot de passe"
+                                            size="lg"
+                                            required
+                                        />
+                                    </Form.Control>
+                                    <Form.Message intent="warning" match="valueMissing">
+                                        Please enter your password
+                                    </Form.Message>
+                                </Form.Field>
+                            </div>
+                            <Form.Submit asChild>
+                                <Button className="w-full" size="lg" label="Se connecter"/>
+                            </Form.Submit>
+                        </Form.Root>
                     </div>
                 </Card>
                 <div className="py-4">
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,16 +1,13 @@
+"use client"
 import Card from "@/components/tailus-ui/Card";
 import Image from "next/image";
-import type {Metadata} from "next";
 import Link from "next/link";
-
-export const metadata: Metadata = {
-    title: "Inscription | Juro",
-    description: "Profiter de tous les avantages de Juro en vous inscrivant !",
-};
+import Form from "@/components/tailus-ui/Form";
+import {Button} from "@/components/tailus-ui/Button";
 
 export default function Register() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-12">
             <Card className="max-w-md w-full p-0 bg-gray-50" variant="mixed">
                 <Card className="-m-px">
                     <div className="mx-auto max-w-[18rem] w-full py-4">
@@ -18,7 +15,61 @@ export default function Register() {
                             <Image src="/icon.svg" width={10} height={10} alt="Juro Logo" className="size-9"/>
                         </a>
                         <h1 className="text-center text-2xl font-semibold text-gray-950 dark:text-white">Rejoignez-nous</h1>
-                        <p className="text-center text-gray-700 dark:text-gray-400">Profiter de tous les avantages de Juro en vous inscrivant !</p>
+                        <p className="text-center text-gray-700 dark:text-gray-400">Profiter de tous les avantages de Juro</p>
+
+                        <Form.Root>
+                            <div className="mb-6 mt-4 space-y-4">
+                                <Form.Field name="name">
+                                    <Form.Control asChild>
+                                        <Form.Input
+                                            variant="outlined"
+                                            type="text"
+                                            size="lg"
+                                            placeholder="Votre nom complet"
+                                            aria-label="Nom complet"
+                                            required
+                                        />
+                                    </Form.Control>
+                                    <Form.Message intent="warning" match="valueMissing">Veuillez remplir votre nom</Form.Message>
+                                </Form.Field>
+                                <Form.Field name="email">
+                                    <Form.Control asChild>
+                                        <Form.Input
+                                            variant="outlined"
+                                            type="email"
+                                            size="lg"
+                                            autoComplete="new-username"
+                                            placeholder="Votre adresse email"
+                                            aria-label="Email"
+                                            required
+                                        />
+                                    </Form.Control>
+                                    <Form.Message intent="warning" match="valueMissing">
+                                        Please enter your email
+                                    </Form.Message>
+                                    <Form.Message intent="danger" match="typeMismatch">
+                                        Please provide a valid email
+                                    </Form.Message>
+                                </Form.Field>
+                                <Form.Field name="password">
+                                    <Form.Control asChild>
+                                        <Form.Input
+                                            variant="outlined"
+                                            type="text"
+                                            size="lg"
+                                            placeholder="Votre mot de passe"
+                                            aria-label="Mot de passe"
+                                            autoComplete="new-password"
+                                            required
+                                        />
+                                    </Form.Control>
+                                    <Form.Message intent="warning" match="valueMissing">Veuillez remplir ce champ</Form.Message>
+                                </Form.Field>
+                            </div>
+                            <Form.Submit asChild>
+                                <Button className="w-full" size="lg" label="S'incrire"/>
+                            </Form.Submit>
+                        </Form.Root>
                     </div>
                 </Card>
                 <div className="py-4">
