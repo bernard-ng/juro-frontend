e021738c00fc40875f842c1f61e013c4a4e7a7cc
Meschac Irung <meschacirung@gmail.com>
2024-05-20 23:36:49 +0200

    feat [sidebar] : add rename dialog

--- app/components/ChatLink.tsx
+++ app/components/ChatLink.tsx
@@ -7,6 +7,8 @@ import { usePathname } from 'next/navigation'
 import { cn } from "@/lib/utils";
 import { toast } from 'sonner'
 import AlertDialog from "./tailus-ui/AlertDialog";
+import Dialog from "./tailus-ui/Dialog";
+import { FormInput } from "./tailus-ui/Form";
 
 export type ChatLinkProps = {
     href: string;
@@ -17,7 +19,11 @@ export const ChatLink: React.FC<ChatLinkProps> = ({
     href,
     title
 }) => {
+
     const [isAlertOpen, setIsAlertOpen] = useState(false)
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const [inputValue, setInputValue] = useState(title);
+
     const pathname = usePathname()
 
     return (
@@ -42,7 +48,7 @@ export const ChatLink: React.FC<ChatLinkProps> = ({
                     </DropdownMenu.Trigger>
                     <DropdownMenu.Portal>
                         <DropdownMenu.Content variant="soft" intent="gray" className="*:py-3 *:gap-3 dark:bg-gray-800 min-w-fit">
-                            <DropdownMenu.Item>
+                            <DropdownMenu.Item onClick={() => setIsDialogOpen(true)}>
                                 <Pencil className="size-4" />
                                 Renommer
                             </DropdownMenu.Item>
@@ -57,30 +63,76 @@ export const ChatLink: React.FC<ChatLinkProps> = ({
             </div>
         </div>
             
-            <AlertDialog.Root variant="centred" open={isAlertOpen} onOpenChange={() => setIsAlertOpen(false)}>
-                <AlertDialog.Portal>
-                    <AlertDialog.Overlay onClick={() => setIsAlertOpen(false)} className="bg-white/50 dark:bg-gray-950/65" />
-                    <AlertDialog.Content className="max-w-xs p-0 overflow-hidden dark:bg-gray-800">
-                        <div className="p-6">
-                            <AlertDialog.Title>
-                                Supprimer le chat ?
-                            </AlertDialog.Title>
-                            <AlertDialog.Description className="mt-2 mb-0">
-                                En supprimant ce chat, vous perdrez toutes les données associées.
-                            </AlertDialog.Description>
-                        </div>
+        <AlertDialog.Root variant="centred" open={isAlertOpen} onOpenChange={() => setIsAlertOpen(false)}>
+            <AlertDialog.Portal>
+                <AlertDialog.Overlay className="bg-white/50 dark:bg-gray-950/65" />
+                <AlertDialog.Content className="max-w-xs p-0 overflow-hidden dark:bg-gray-800">
+                    <div className="p-6">
+                        <AlertDialog.Title>
+                            Supprimer le chat ?
+                        </AlertDialog.Title>
+                        <AlertDialog.Description className="mt-2 mb-0">
+                            En supprimant ce chat, vous perdrez toutes les données associées.
+                        </AlertDialog.Description>
+                    </div>
 
-                        <AlertDialog.Actions className="border-t divide-x gap-0 mt-0 dark:border-gray-700 dark:divide-gray-700">
-                            <AlertDialog.Cancel asChild>
-                                <Button className="w-1/2 justify-center rounded-none" size="lg" variant="ghost" colorVariant="gray" label="Annuler" />
-                            </AlertDialog.Cancel>
-                            <AlertDialog.Action asChild>
-                                <Button className="w-1/2 justify-center rounded-none" size="lg" variant="ghost" colorVariant="danger" label="Supprimer" onClick={() => toast.success('Chat supprimé avec succès')} />
-                            </AlertDialog.Action>
-                        </AlertDialog.Actions>
-                    </AlertDialog.Content>
-                </AlertDialog.Portal>
-            </AlertDialog.Root>
+                    <AlertDialog.Actions className="border-t divide-x gap-0 mt-0 dark:border-gray-700 dark:divide-gray-700">
+                        <AlertDialog.Cancel asChild>
+                            <Button className="w-1/2 justify-center rounded-none" size="lg" variant="ghost" colorVariant="gray" label="Annuler" />
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action asChild>
+                            <Button className="w-1/2 justify-center rounded-none" size="lg" variant="ghost" colorVariant="danger" label="Supprimer" onClick={() => toast.success('Chat supprimé avec succès')} />
+                        </AlertDialog.Action>
+                    </AlertDialog.Actions>
+                </AlertDialog.Content>
+            </AlertDialog.Portal>
+        </AlertDialog.Root>
+            
+        <Dialog.Root open={isDialogOpen} onOpenChange={() => setIsDialogOpen(false)}>
+            <Dialog.Portal>
+                <Dialog.Overlay className="bg-white/50 dark:bg-gray-950/65" />
+                <Dialog.Content className="p-6 dark:bg-gray-800">
+                    <Dialog.Title className="text-lg">Renommer le Chat</Dialog.Title>
+                    
+                        <form
+                            className="mt-4"
+                            id="renameChatForm"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                            }}
+                        >
+                        <FormInput
+                            type="text"
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                            variant="outlined"
+                            size="lg"
+                            className="hover:border-gray-300 dark:border-gray-700 dark:hover:border-gray-600"
+                        />  
+                    </form>
+                    
+                    <Dialog.Actions className="gap-0">
+                        <Dialog.Close asChild>
+                            <Button label="Annuler" variant="ghost" colorVariant="primary" size="md"/>
+                        </Dialog.Close>
+                        <Button
+                            type="submit"
+                            form="renameChatForm"
+                            label="Renommer"
+                            variant="ghost"
+                            colorVariant="primary"
+                            size="md"
+                            disabled={inputValue.trim() === title.trim()}
+                            onClick={() => {
+                                setIsDialogOpen(false);
+                                toast.success(`Chat renommé à : "${inputValue}"`);
+                                setInputValue(title);
+                            }}
+                        />
+                    </Dialog.Actions>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
         </>
     )
 }
---
+++ app/components/tailus-ui/Dialog.tsx
@@ -0,0 +1,100 @@
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import {dialog as dialogTheme} from "@tailus/themer-dialog";
+import { cn } from "lib/utils";
+import React from "react";
+import {Button} from "./Button";
+
+const DialogRoot = DialogPrimitive.Root;
+const DialogTrigger = DialogPrimitive.Trigger;
+const DialogPortal = DialogPrimitive.Portal;
+const DialogOverlay = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Overlay>,
+  React.ComponentProps<typeof DialogPrimitive.Overlay>
+>(({className, ...props}, forwardedRef) => (
+  <DialogPrimitive.Overlay
+    {...props}
+    ref={forwardedRef}
+    className={cn(dialogTheme.overlay, className)}
+  />
+));
+
+const DialogContent = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentProps<typeof DialogPrimitive.Content>
+>(({className, ...props}, forwardedRef) => (
+  <DialogPrimitive.Content
+    {...props}
+    ref={forwardedRef}
+    className={cn(dialogTheme.content, className)}
+  />
+));
+
+const DialogTitle = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Title>,
+  React.ComponentProps<typeof DialogPrimitive.Title>
+>(({className, ...props}, forwardedRef) => (
+  <DialogPrimitive.Title
+    {...props}
+    ref={forwardedRef}
+    className={cn(dialogTheme.title, className)}
+  />
+));
+
+const DialogDescription = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Description>,
+  React.ComponentProps<typeof DialogPrimitive.Description>
+>(({className, ...props}, forwardedRef) => (
+  <DialogPrimitive.Description
+    {...props}
+    ref={forwardedRef}
+    className={cn(dialogTheme.description, className)}
+  />
+));
+
+const DialogActions = React.forwardRef<
+  React.ElementRef<"div">,
+  React.ComponentProps<"div">
+>(({className, ...props}, forwardedRef) => (
+  <div
+    {...props}
+    ref={forwardedRef}
+    className={cn(dialogTheme.actions, className)}
+  />
+));
+
+type DialogCloseButtonProps = React.ComponentProps<typeof Button>;
+
+const DialogCloseButton: React.FC<DialogCloseButtonProps> = ({className, ...props}) => (
+  <Button
+    {...props}
+    className={cn(dialogTheme.closeButton, className)}
+  />
+);
+
+const DialogClose = DialogPrimitive.Close;
+
+export default {
+  Root: DialogRoot,
+  Trigger: DialogTrigger,
+  Portal: DialogPortal,
+  Overlay: DialogOverlay,
+  Content: DialogContent,
+  Title: DialogTitle,
+  Description: DialogDescription,
+  Actions: DialogActions,
+  CloseButton: DialogCloseButton,
+  Close: DialogClose,
+}
+
+export {
+  DialogRoot,
+  DialogTrigger,
+  DialogPortal,
+  DialogOverlay,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogActions,
+  DialogCloseButton,
+  DialogClose,
+}
