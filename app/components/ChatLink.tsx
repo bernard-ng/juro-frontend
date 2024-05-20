9eb4a9f6a49a12d7d4157879fe9afa93a3186972
Meschac Irung <meschacirung@gmail.com>
2024-05-20 22:33:03 +0200

    feat [sidebar] : add chat delete alert dialog

--- app/components/ChatLink.tsx
+++ app/components/ChatLink.tsx
@@ -1,11 +1,12 @@
 import DropdownMenu from "@tailus-ui/DropdownMenu";
 import { Button } from "@tailus-ui/Button";
-import { EllipsisVertical, Pencil, Share, Trash } from "lucide-react";
+import { EllipsisVertical, Pencil, Trash } from "lucide-react";
 import Link from "next/link";
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { usePathname } from 'next/navigation'
 import { cn } from "@/lib/utils";
 import { toast } from 'sonner'
+import AlertDialog from "./tailus-ui/AlertDialog";
 
 export type ChatLinkProps = {
     href: string;
@@ -15,106 +16,71 @@ export type ChatLinkProps = {
 export const ChatLink: React.FC<ChatLinkProps> = ({
     href,
     title
-}) =>{
-    const [isRenaming, setIsRenaming] = useState(false);
-    const linkRef = useRef<HTMLAnchorElement>(null);
-    const saveButtonRef = useRef<HTMLButtonElement>(null);
+}) => {
+    const [isAlertOpen, setIsAlertOpen] = useState(false)
     const pathname = usePathname()
 
-    useEffect(() => {
-
-        if (isRenaming && linkRef.current && saveButtonRef.current) {
-            linkRef?.current.focus();
-        }
-
-        const handleClickOutside = (e: MouseEvent) => {
-            if (linkRef.current && !linkRef.current.contains(e.target as Node) && saveButtonRef.current && !saveButtonRef.current.contains(e.target as Node)) {
-                setIsRenaming(false);
-            }
-        }
-
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [isRenaming]);
-
     return (
-        <div
-            className={
-                cn(
-                    "relative group rounded-[--btn-border-radius] hover:bg-gray-100 h-9 dark:hover:bg-gray-900 has-[[data-state='open']]:bg-gray-100 dark:has-[[data-state='open']]:bg-gray-900",
-                    isRenaming && "z-10"
-                )
-            }
-        >
+        <>
+        <div className="relative group rounded-[--btn-border-radius] hover:bg-gray-100 h-9 dark:hover:bg-gray-900 has-[[data-state='open']]:bg-gray-100 dark:has-[[data-state='open']]:bg-gray-900">
             <Link
-                ref={linkRef}
                 href={href}
-                contentEditable={isRenaming}
-                onClick={(event) => {
-                    if (isRenaming) {
-                        event.preventDefault();
-                    }
-                }}
-                className={
-                    cn(
+                className={cn(
                         "rounded-[--btn-border-radius] h-full w-full flex items-center px-4 text-sm text-gray-700 dark:text-gray-300 focus-visible:outline outline-2 outline-primary-600 text-nowrap",
                         pathname.includes(href) && "bg-gray-100 dark:bg-gray-900"
                     )
-                }>
+                }
+            >
                 {title}
             </Link>
-            {
-                isRenaming &&
-                <Button
-                    ref={saveButtonRef}
-                    icon="only"
-                    size="sm"
-                    variant="soft"
-                    colorVariant="primary"
-                    label="Enregistrer"
-                    onClick={
-                        () => {
-                            console.log(isRenaming);
-                            toast.error('Error message');
-                            toast.success('Le chat a été renommé avec succès !'); 
-                        }
-                    }
-                    className="z-10 absolute inset-y-0.5 right-0.5 dark:bg-gray-800 dark:text-primary-400"
-                >
-                    <svg className="!size-5" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"><path fill="currentColor" d="M3 5a2 2 0 0 1 2-2h1v3.5A1.5 1.5 0 0 0 7.5 8h4A1.5 1.5 0 0 0 13 6.5V3h.379a2 2 0 0 1 1.414.586l1.621 1.621A2 2 0 0 1 17 6.621V15a2 2 0 0 1-2 2v-5.5a1.5 1.5 0 0 0-1.5-1.5h-7A1.5 1.5 0 0 0 5 11.5V17a2 2 0 0 1-2-2zm9-2H7v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5zm2 8.5V17H6v-5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5"></path></svg>
-                </Button>
-            }
-            {
-                !isRenaming &&
-                <div className="absolute inset-y-0.5 my-auto right-0.5 hidden group-hover:block has-[[data-state='open']]:block">
-                    <DropdownMenu.Root>
-                        <DropdownMenu.Trigger asChild className="data-[state=open]:bg-white dark:data-[state=open]:bg-gray-800">
-                            <Button icon="only" size="sm" variant="ghost" colorVariant="gray" label="Menu du chat" className="focus-visible:outline outline-2 outline-primary-600 outline-offset-2">
-                                <EllipsisVertical />
-                            </Button>
-                        </DropdownMenu.Trigger>
-                        <DropdownMenu.Portal>
-                        <DropdownMenu.Content variant="soft" intent="gray" className="*:py-3 *:gap-3 dark:bg-gray-800 dark:border-gray-700/75 min-w-fit">
+            <div className="absolute inset-y-0.5 my-auto right-0.5 hidden group-hover:block has-[[data-state='open']]:block">
+                <DropdownMenu.Root>
+                    <DropdownMenu.Trigger asChild className="data-[state=open]:bg-white dark:data-[state=open]:bg-gray-800">
+                        <Button icon="only" size="sm" variant="ghost" colorVariant="gray" label="Menu du chat" className="focus-visible:outline outline-2 outline-primary-600 outline-offset-2">
+                            <EllipsisVertical />
+                        </Button>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Portal>
+                        <DropdownMenu.Content variant="soft" intent="gray" className="*:py-3 *:gap-3 dark:bg-gray-800 min-w-fit">
                             <DropdownMenu.Item>
-                                <Share className="size-4" />
-                                Partager
-                            </DropdownMenu.Item>
-                            <DropdownMenu.Item onClick={() => setIsRenaming(true)}>
                                 <Pencil className="size-4" />
                                 Renommer
                             </DropdownMenu.Item>
                             <DropdownMenu.Separator className="!py-0 dark:bg-gray-700/75"/>
-                            <DropdownMenu.Item intent="danger">
+                            <DropdownMenu.Item intent="danger" onClick={() => setIsAlertOpen(true)}>
                                 <Trash className="size-4"/>
                                 Supprimer
                             </DropdownMenu.Item>
                         </DropdownMenu.Content>
-                        </DropdownMenu.Portal>
-                    </DropdownMenu.Root>
-                </div>
-            }
+                    </DropdownMenu.Portal>
+                </DropdownMenu.Root>
+            </div>
         </div>
+            
+            <AlertDialog.Root variant="centred" open={isAlertOpen} onOpenChange={() => setIsAlertOpen(false)}>
+                <AlertDialog.Portal>
+                    <AlertDialog.Overlay onClick={() => setIsAlertOpen(false)} className="bg-white/50 dark:bg-gray-950/65" />
+                    <AlertDialog.Content className="max-w-xs p-0 overflow-hidden dark:bg-gray-800">
+                        <div className="p-6">
+                            <AlertDialog.Title>
+                                Supprimer le chat ?
+                            </AlertDialog.Title>
+                            <AlertDialog.Description className="mt-2 mb-0">
+                                En supprimant ce chat, vous perdrez toutes les données associées.
+                            </AlertDialog.Description>
+                        </div>
+
+                        <AlertDialog.Actions className="border-t divide-x gap-0 mt-0 dark:border-gray-700 dark:divide-gray-700">
+                            <AlertDialog.Cancel asChild>
+                                <Button className="w-1/2 justify-center rounded-none" size="lg" variant="ghost" colorVariant="gray" label="Annuler" />
+                            </AlertDialog.Cancel>
+                            <AlertDialog.Action asChild>
+                                <Button className="w-1/2 justify-center rounded-none" size="lg" variant="ghost" colorVariant="danger" label="Supprimer" onClick={() => toast.success('Chat supprimé avec succès')} />
+                            </AlertDialog.Action>
+                        </AlertDialog.Actions>
+                    </AlertDialog.Content>
+                </AlertDialog.Portal>
+            </AlertDialog.Root>
+        </>
     )
 }
