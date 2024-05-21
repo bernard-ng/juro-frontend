20fae6402e2cac47c5d31b00943865a71fa444e8
Meschac Irung <meschacirung@gmail.com>
2024-05-21 08:34:56 +0200

    add settings and privacy links

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,7 +1,8 @@
 "use client"
 import Link from "next/link";
-import { PlusIcon} from "lucide-react";
+import { Info, PlusIcon, Settings} from "lucide-react";
 import { ChatLink, ChatLinkProps } from "./ChatLink";
+import { ghostLeadingIconButton } from "@tailus/themer-button";
 
 const links: ChatLinkProps[] = [
   {
@@ -20,7 +21,7 @@ const links: ChatLinkProps[] = [
 
 export function Sidebar() {
   return (
-    <aside className="fixed left-0 px-4 pt-20 inset-y-0 border-r w-64 bg-white dark:bg-gray-900 dark:border-transparent">
+    <aside className="fixed flex flex-col left-0 px-4 pt-20 pb-6 inset-y-0 border-r w-64 bg-white dark:bg-gray-900 dark:border-transparent">
         <Link href="/" className="h-9 rounded-[--btn-border-radius] flex gap-3 items-center font-normal px-3 text-sm border shadow-sm shadow-gray-950/5 text-gray-950 hover:bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-900 dark:text-white dark:border-gray-800">
             <PlusIcon className="size-4" />
             <span>Nouveau chat</span>
@@ -28,7 +29,7 @@ export function Sidebar() {
 
         <div className="mt-6">
             <span className="block ml-4 text-gray-600 dark:text-gray-400 text-sm">Récents</span>
-            <div className="space-y-px mt-2">
+            <div className="space-y-px mt-2 max-h-96 overflow-y-auto">
               {
                 links.map((link, index) => (
                   <ChatLink key={index} href={link.href} title={link.title} />
@@ -36,6 +37,18 @@ export function Sidebar() {
               }
             </div>
         </div>
+      
+      <div className="mt-auto">
+        <Link className={ghostLeadingIconButton.gray.md} href="/confidentialite">
+          <Info className="size-4" />
+          <span className="text-sm ml-1">Confidentialité</span>
+        </Link>
+
+        <Link className={ghostLeadingIconButton.gray.md} href="/parametres">
+          <Settings className="size-4" />
+          <span className="text-sm ml-1">Parametres</span>
+        </Link>
+      </div>
     </aside>
   );
 }
