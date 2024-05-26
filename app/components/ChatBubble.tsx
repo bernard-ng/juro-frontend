4286082aaa4be2e4b116e46f5f246dde2fd38bbf
bernard-ng <ngandubernard@gmail.com>
2024-05-26 17:19:06 +0200

    enhance chat bubble

--- app/components/ChatBubble.tsx
+++ app/components/ChatBubble.tsx
@@ -6,13 +6,11 @@ export function ChatBubble({message} : {message: Message}) {
     
     const UserBubble = (message: Message) => {
         return (
-            <div className="flex items-end gap-2.5 mb-8">
-                <div className="flex flex-col w-full max-w-[400px] leading-1.5">
-                    <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                        <span className="text-sm font-semibold text-gray-900 dark:text-white">Vous</span>
-                        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">{message.created_at}</span>
+            <div className="flex items-start justify-end gap-2.5 mb-8">
+                <div className="flex flex-col gap-1 w-full max-w-[350px]">
+                    <div className="flex flex-col leading-1.5 p-4 border-gray-200 rounded-xl bg-gray-100 dark:bg-gray-800">
+                        <p className="text-sm font-normal text-gray-900 dark:text-white whitespace-break-spaces">{message.message}</p>
                     </div>
-                    <p className="text-sm font-normal py-2 text-gray-900 dark:text-white">{message.message}</p>
                 </div>
             </div>
         )
@@ -21,15 +19,13 @@ export function ChatBubble({message} : {message: Message}) {
     const BotBubble = (message: Message) => {
         return (
             <div className="flex items-start gap-2.5 mb-8">
-                <Image src="/icon.svg" width={10} height={10} alt="Juro Logo" className="size-9"/>
-                <div className="flex flex-col gap-1 w-full max-w-[400px]">
+                <Image src="/icon.svg" width={8} height={8} alt="Juro Logo" className="size-6"/>
+                <div className="flex flex-col gap-1 w-full max-w-full">
                     <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                        <span className="text-sm font-semibold text-gray-900 dark:text-white">Juro</span>
-                        <span className="text-sm font-normal text-gray-500 dark:text-gray-400">{message.created_at}</span>
+                        <span className="text-sm font-bold text-gray-900 dark:text-white">Juro</span>
                     </div>
-                    <div
-                        className="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-                        <p className="text-sm font-normal text-gray-900 dark:text-white">{message.message}</p>
+                    <div className="flex flex-col leading-1.5">
+                        <p className="text-sm font-normal py-2 text-gray-900 dark:text-white whitespace-break-spaces">{message.message}</p>
                     </div>
                 </div>
             </div>
