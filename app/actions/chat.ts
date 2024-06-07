ff76c14ee612b52d9e3288edc2658cfd0d06e200
bernard-ng <ngandubernard@gmail.com>
2024-06-07 02:14:20 +0200

    remove chat action server

--- app/actions/chat.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-import {fetchApi, getRequestHeaders} from "@lib/api/api";
-import { cookies } from 'next/headers'
-
-const token = cookies().get('token')?.value
-const headers = getRequestHeaders(token as string)
-
-export async function createChat() {
-}
